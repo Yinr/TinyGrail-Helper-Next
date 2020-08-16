f0ba40ea408a5e64351bd08fe9e1dc82590cb5fb
Yinr <yinr@yinr.cc>
2020-08-16 13:08:21 +0800

    增加复制我的ICO链接功能

--- TinyGrail-Helper-Next.user.js
+++ TinyGrail-Helper-Next.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TinyGrail Helper Next
 // @namespace    https://gitee.com/Yinr/TinyGrail-Helper-Next
-// @version      3.0.3
+// @version      3.0.4
 // @description  为小圣杯增加一些小功能,讨论/反馈：https://bgm.tv/group/topic/353368
 // @author       Liaune,Cedar,Yinr
 // @include     /^https?://(bgm\.tv|bangumi\.tv|chii\.in)/(user|character|rakuen\/topiclist|rakuen\/home|rakuen\/topic\/crt).*
@@ -528,6 +528,40 @@ function loadMyICO(page){
   getData(`chara/user/initial/0/${page}/50`).then((d)=>{
     if (d.State == 0) {
       loadCharacterList(d.Value.Items,d.Value.CurrentPage, d.Value.TotalPages, loadMyICO, 'ico',false);
+      if(d.Value.TotalItems > 0 && page == 1){
+        $('#eden_tpc_list ul').prepend(`<li id="copyICO" class="line_odd item_list item_function" style="text-align: center;">[复制我的 ICO]</li>`);
+        $('#copyICO').on('click', function () {
+          getData(`chara/user/initial/0/1/1000`).then(d => {
+            let list_text = d.Value.Items.map(i => `https://bgm.tv/character/${i.CharacterId} ${i.Name}`).join('\n');
+            closeDialog();
+            let dialog = `<div id="TB_overlay" class="TB_overlayBG TB_overlayActive"></div>
+<div id="TB_window" class="dialog" style="display:block;max-width:640px;min-width:400px;">
+<div class="bibeBox" style="padding:10px">
+<label>我的 ICO 列表（若复制按钮无效，请手动复制）</label>
+<textarea rows="10" class="quick" name="myICO"></textarea>
+<input class="inputBtn" value="复制" id="copy_list" type="submit" style="padding: 3px 5px;">
+</div>
+<a id="TB_closeWindowButton" title="Close">X关闭</a>
+</div>
+</div>`;
+            $('body').append(dialog);
+            $('#TB_closeWindowButton').on('click', closeDialog);
+            $('#TB_overlay').on('click', closeDialog);
+
+            $('.bibeBox textarea').val(list_text);
+            $('#copy_list').on('click', () => {
+              $('.bibeBox label').children().remove();
+              let res_info = '复制 ICO 列表出错，请手动复制';
+              $('.bibeBox textarea').select();
+              try {
+                if (document.execCommand('copy'))
+                  res_info = `已复制 ${list_text.split('\n').length} 个 ICO`;
+              } catch (e) {console.log('复制 ICO 列表出错', e);}
+              $('.bibeBox label').append(`<br><span>${res_info}</span>`);
+            });
+          });
+        });
+      }
     }
   });
 }
@@ -537,7 +571,7 @@ function loadMyTemple(page){
     if (d.State == 0) {
       loadCharacterList(d.Value.Items,d.Value.CurrentPage, d.Value.TotalPages, loadMyTemple, 'temple',false);
       if(page == 1){
-        $('#eden_tpc_list ul').prepend(`<li id="lostTemple" class="line_odd item_list" style="text-align: center;">[查看受损圣殿]</li>`);
+        $('#eden_tpc_list ul').prepend(`<li id="lostTemple" class="line_odd item_list item_function" style="text-align: center;">[查看受损圣殿]</li>`);
         $('#lostTemple').on('click', function () {
           $('#eden_tpc_list ul').html('');
           loadLostTemple(1);
