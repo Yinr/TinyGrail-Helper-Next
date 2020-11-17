8ea704c7d2e2f93fa3ef9134f53dd6257c6ad220
Yinr <yinr@yinr.cc>
2020-11-17 10:19:48 +0000

    Release v3.1.20

--- gadgets/script.js
+++ gadgets/script.js
@@ -5,7 +5,7 @@
 // @include     http*://bgm.tv/*
 // @include     http*://bangumi.tv/*
 // @include     http*://chii.in/*
-// @version     3.1.19
+// @version     3.1.20
 // @author      Liaune, Cedar, no1xsyzy(InQβ), Yinr
 // @homepage    https://github.com/Yinr/TinyGrail-Helper-Next
 // @license     MIT
@@ -2022,6 +2022,46 @@
     $('#myTemple').on('click', () => menuItemClicked(loadMyTemple, tinygrailMenuId));
   };
 
+  const markFollow = () => {
+    const followInfoTagsClass = 'item-info-tags';
+    const followInfoTag = `<small class="${followInfoTagsClass}"></small>`;
+    const followChara = '<div title="已关注角色" class="item-info-tag item-info-chara-icon"></div>';
+    const followAuc = '<div title="已关注拍卖" class="item-info-tag item-info-auc-icon"></div>';
+    const followIco = '<div title="已自动补款" class="item-info-tag item-info-ico-icon"></div>';
+    const followTemple = '<div title="已自动建塔" class="item-info-tag item-info-temple-icon"></div>';
+    $('.item_list').each((_, el) => {
+      const itemEl = $(el);
+      let id = itemEl.data('id');
+      if (!id) {
+        const avatarUrl = itemEl.find('a.avatar').attr('href');
+        const recMatch = itemEl.find('.row .time').text().match(/#(\d+)/) || ['', ''];
+        id = parseInt(avatarUrl ? avatarUrl.match(/topic\/crt\/(\d+)([?/]|$)/)[1] : recMatch[1]);
+      }
+      let followInfoTagEl = itemEl.find(`.${followInfoTagsClass}`);
+      if (!followInfoTagEl.length) followInfoTagEl = itemEl.find('.inner .row').before(followInfoTag).prev();
+      if (!id || !followInfoTagEl) return
+      let followInfo = '';
+      const followList = FollowList.get();
+      if (followList.charas.includes(id)) followInfo += followChara;
+      if (followList.auctions.includes(id)) followInfo += followAuc;
+      const templeInfo = AutoTempleList.get().find(e => parseInt(e.charaId) === id);
+      if (templeInfo) {
+        followInfo += followTemple;
+        if (templeInfo.bidPrice && templeInfo.target) {
+          followInfo += `<small title="自动建塔价 × 数量">(${templeInfo.bidPrice} * ${templeInfo.target})</small>`;
+        }
+      }
+      const fillIcoInfo = FillICOList.get().find(e => parseInt(e.charaId) === id);
+      if (fillIcoInfo) {
+        followInfo += followIco;
+        if (fillIcoInfo.target) {
+          followInfo += `<small title="自动补款目标">(lv${fillIcoInfo.target})</small>`;
+        }
+      }
+      followInfoTagEl.html(followInfo);
+    });
+  };
+
   const openBuildDialog = (chara) => {
     const autoTempleList = AutoTempleList.get();
     const charaId = chara.CharacterId || chara.Id;
@@ -2792,6 +2832,16 @@
         }
       });
     }
+    launchObserver({
+      parentNode: document.body,
+      selector: 'ul .load_more',
+      successCallback: (mutationList) => {
+        if (mutationList.some(muRecord => Array.from(muRecord.addedNodes.values()).some(el => $(el).is('.load_more')))) {
+          markFollow();
+        }
+      },
+      stopWhenSuccess: false
+    });
   } else
   if (location.pathname.startsWith('/user')) {
     launchObserver({
