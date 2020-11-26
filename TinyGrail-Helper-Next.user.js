c22489452ba3ca698ae680f0980ae44cb036437b
Yinr <yinr@yinr.cc>
2020-11-26 08:30:39 +0000

    Release v3.1.24

--- TinyGrail-Helper-Next.user.js
+++ TinyGrail-Helper-Next.user.js
@@ -5,7 +5,7 @@
 // @include     http*://bgm.tv/*
 // @include     http*://bangumi.tv/*
 // @include     http*://chii.in/*
-// @version     3.1.23
+// @version     3.1.24
 // @author      Liaune, Cedar, no1xsyzy(InQβ), Yinr
 // @homepage    https://github.com/Yinr/TinyGrail-Helper-Next
 // @license     MIT
@@ -116,6 +116,9 @@
       s[1] += new Array(prec - s[1].length + 1).join('0');
     }
     return s.join(dec)
+  };
+  const formatMoney = (number) => {
+    return '₵' + formatNumber(number, 2)
   };
   const formatAskPrice = (price) => {
     if (Number.isInteger(parseFloat(price))) return price
@@ -2043,6 +2046,145 @@
     });
   };
 
+  const bindShowBidsTotal = () => {
+    $('#bidMenu').on('click', () => {
+      $('#bidsTotalInfo').remove();
+      $('#eden_tpc_list ul').prepend('<li id="bidsTotalInfo" class="line_odd item_list item_function" style="text-align: center;">[统计买单总额]</li>');
+      const calculateBidsTotal = () => {
+        $('#bidsTotalInfo').off('click').text('[买单统计中，请稍候...]');
+        getAllBids().then(res => {
+          if (res) {
+            $('#bidsTotalInfo').text(res);
+          } else {
+            $('#bidsTotalInfo').text('[买单统计出错，点击重试]').on('click', calculateBidsTotal);
+          }
+        });
+      };
+      $('#bidsTotalInfo').on('click', calculateBidsTotal);
+    });
+  };
+  const getAllBids = async () => {
+    let bidsRes = await getData('chara/bids/0/1/50').catch(e => console.warn('统计买单总额时获取买单信息失败', e));
+    if (!bidsRes) return false
+    if (bidsRes.State === 0 && bidsRes.Value) {
+      if (bidsRes.Value.TotalPages > 1) {
+        const totalItems = bidsRes.Value.TotalItems;
+        bidsRes = await getData(`chara/bids/0/1/${totalItems}`).catch(e => console.warn('统计买单总额时获取买单信息失败', e));
+        if (!bidsRes) return false
+      }
+      const bidsCharas = [];
+      bidsRes.Value.Items.forEach(chara => {
+        if (!bidsCharas.some(i => i.CharacterId === chara.CharacterId)) bidsCharas.push(chara);
+      });
+      return await sumBids(bidsCharas)
+    } else if (bidsRes.State !== 0) {
+      console.warn(bidsRes);
+      return false
+    }
+  };
+  const sumBids = async (charas) => {
+    let sum = 0;
+    let count = 0;
+    for (let i = 0; i < charas.length; i++) {
+      const d = await getData(`chara/user/${charas[i].CharacterId}`).catch(e => console.warn(e));
+      if (d.State === 0) {
+        sum += d.Value.Bids.reduce((s, i) => s + i.Price * i.Amount, 0);
+        count += d.Value.Bids.length;
+      } else {
+        console.warn(d);
+      }
+    }
+    return `共有 ${formatNumber(count, 0)} 买单，涉及 ${formatNumber(charas.length, 0)} 角色，合计 ${formatMoney(sum)}`
+  };
+
+  const bindShowAsksTotal = () => {
+    $('#askMenu').on('click', () => {
+      $('#asksTotalInfo').remove();
+      $('#eden_tpc_list ul').prepend('<li id="asksTotalInfo" class="line_odd item_list item_function" style="text-align: center;">[统计卖单总数]</li>');
+      const calculateAsksTotal = () => {
+        $('#asksTotalInfo').off('click').text('[卖单统计中，请稍候...]');
+        getAllAsks().then(res => {
+          if (res) {
+            $('#asksTotalInfo').text(res);
+          } else {
+            $('#asksTotalInfo').text('[卖单统计出错，点击重试]').on('click', calculateAsksTotal);
+          }
+        });
+      };
+      $('#asksTotalInfo').on('click', calculateAsksTotal);
+    });
+  };
+  const getAllAsks = async () => {
+    let asksRes = await getData('chara/asks/0/1/50').catch(e => console.warn('统计卖单总数时获取卖单信息失败', e));
+    if (!asksRes) return false
+    if (asksRes.State === 0 && asksRes.Value) {
+      if (asksRes.Value.TotalPages > 1) {
+        const totalItems = asksRes.Value.TotalItems;
+        asksRes = await getData(`chara/asks/0/1/${totalItems}`).catch(e => console.warn('统计卖单总数时获取卖单信息失败', e));
+        if (!asksRes) return false
+      }
+      const asksCharas = [];
+      asksRes.Value.Items.forEach(chara => {
+        if (!asksCharas.some(i => i.CharacterId === chara.CharacterId)) asksCharas.push(chara);
+      });
+      return await sumAsks(asksCharas)
+    } else if (asksRes.State !== 0) {
+      console.warn(asksRes);
+      return false
+    }
+  };
+  const sumAsks = async (charas) => {
+    let sum = 0;
+    let count = 0;
+    for (let i = 0; i < charas.length; i++) {
+      const d = await getData(`chara/user/${charas[i].CharacterId}`).catch(e => console.warn(e));
+      if (d.State === 0) {
+        sum += d.Value.Asks.reduce((s, i) => s + i.Amount, 0);
+        count += d.Value.Asks.length;
+      } else {
+        console.warn(d);
+      }
+    }
+    return `共有 ${formatNumber(count, 0)} 卖单，涉及 ${formatNumber(charas.length, 0)} 角色，合计 ${formatNumber(sum, 0)} 股`
+  };
+
+  const bindShowAuctionTotal = () => {
+    $('#auctionMenu').on('click', () => {
+      $('#auctionTotalInfo').remove();
+      $('#eden_tpc_list ul').prepend('<li id="auctionTotalInfo" class="line_odd item_list item_function" style="text-align: center;">[统计拍卖总额]</li>');
+      const calculateAuctionTotal = () => {
+        $('#auctionTotalInfo').off('click').text('[拍卖统计中，请稍候...]');
+        getAllAuction().then(res => {
+          if (res) {
+            $('#auctionTotalInfo').text(res);
+          } else {
+            $('#auctionTotalInfo').text('[拍卖统计出错，点击重试]').on('click', calculateAuctionTotal);
+          }
+        });
+      };
+      $('#auctionTotalInfo').on('click', calculateAuctionTotal);
+    });
+  };
+  const getAllAuction = async () => {
+    let page = 0;
+    const auctionCharas = [];
+    let retry = false;
+    do {
+      if (!retry) page++;
+      const auctionRes = await getData(`chara/user/auction/${page}/50`).catch(e => console.warn(`统计拍卖总额时获取第${page}页拍卖信息失败`, e));
+      if (auctionRes) {
+        retry = false;
+        if (auctionRes.State === 0 && auctionRes.Value) {
+          auctionCharas.push(...auctionRes.Value.Items.filter(i => i.State === 0));
+        } else if (auctionRes.State !== 0) {
+          console.warn(auctionRes);
+        }
+      } else { retry = true; }
+    } while (retry || auctionCharas.length >= 50 * page)
+    const sum = auctionCharas.reduce((s, i) => s + i.Price * i.Amount, 0);
+    return `共参与 ${formatNumber(auctionCharas.length, 0)} 角色竞拍，合计 ${formatMoney(sum)}`
+  };
+
   const menuItemClicked = (callback, parentNodeId = '#helperMenu') => {
     $('.timelineTabs a').removeClass('focus');
     $('.timelineTabs a').removeClass('top_focus');
@@ -2082,6 +2224,9 @@
     const tinygrailMenuId = '#recentMenu';
     $('#myICO').on('click', () => menuItemClicked(loadMyICO, tinygrailMenuId));
     $('#myTemple').on('click', () => menuItemClicked(loadMyTemple, tinygrailMenuId));
+    bindShowBidsTotal();
+    bindShowAsksTotal();
+    bindShowAuctionTotal();
   };
 
   const markFollow = () => {
