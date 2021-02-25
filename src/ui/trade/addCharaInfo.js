4874e4a59b7ab9855af17897e308394fccb49cde
Yinr <yinr@yinr.cc>
2021-02-25 19:57:54 +0800

    去除旧版股息计算

--- src/ui/trade/addCharaInfo.js
+++ src/ui/trade/addCharaInfo.js
@@ -399,27 +399,12 @@ const showPrice = (chara) => {
 
 const showTempleRate = (chara) => {
   const charaId = chara.CharacterId || chara.Id
-  const rate = chara.Rate
-  const level = chara.Level
   getData(`chara/temple/${chara.Id}`).then((d) => {
     const templeAll = { 1: 0, 2: 0, 3: 0 }
     for (let i = 0; i < d.Value.length; i++) {
       templeAll[d.Value[i].Level]++
     }
-    const templeRate = rate * (level + 1) * 0.3
-    $(`#grailBox.chara${charaId} .assets_box .bold .sub`).attr('title', '活股股息:' + formatNumber(rate, 2))
     $(`#grailBox.chara${charaId} .assets_box .bold .sub`).before(`<span class="sub"> (${templeAll[3]} + ${templeAll[2]} + ${templeAll[1]})</span>`)
-    if ($(`#grailBox.chara${charaId} #expandButton`).length) {
-      $(`#grailBox.chara${charaId} #expandButton`).before(`<span class="sub" title="圣殿股息:${formatNumber(templeRate, 2)}"> (${formatNumber(templeRate, 2)})</span>`)
-    } else {
-      launchObserver({
-        parentNode: document.querySelector(`#grailBox.chara${charaId}`),
-        selector: `#grailBox.chara${charaId} #expandButton`,
-        successCallback: () => {
-          $(`#grailBox.chara${charaId} #expandButton`).before(`<span class="sub" title="圣殿股息:${formatNumber(templeRate, 2)}"> (${formatNumber(templeRate, 2)})</span>`)
-        }
-      })
-    }
   })
 }
 
