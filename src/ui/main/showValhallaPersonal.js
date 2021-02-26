8a426cd449e6f930bfd845c043574eda595f6552
Yinr <yinr@yinr.cc>
2021-02-26 17:04:14 +0800

    修复英灵殿献祭值不计算连接的问题，增加损耗值显示

--- src/ui/main/showValhallaPersonal.js
+++ src/ui/main/showValhallaPersonal.js
@@ -7,9 +7,23 @@ export const showValhallaPersonal = async (itemList) => {
     const charaInfo = await getData(`chara/user/${id}`)
     const amount = charaInfo.Value.Amount
     const userId = charaInfo.Value.Id
+
+    let sacrifices = 0
+    let assets = 0
     const templeInfo = await getData(`chara/temple/${id}`)
-    const temple = templeInfo.Value.find(i => i.UserId === userId) || {}
-    const sacrifices = temple.Sacrifices || 0
-    chara.find('a.avatar[data-id] > img').after(`<div style="text-align: center; line-height: 1em;" title="持股数 | 献祭值"><small>${amount} | ${sacrifices}</small></div>`)
+    const temple = templeInfo.Value.find(i => i.UserId === userId)
+    if (temple === undefined) {
+      const linkInfo = await getData(`chara/links/${id}`)
+      const link = linkInfo.Value.find(i => i.UserId === userId) || {}
+      if (link !== undefined) {
+        sacrifices = link.Sacrifices || 0
+        assets = link.Assets || 0
+      }
+    } else {
+      sacrifices = temple.Sacrifices || 0
+      assets = temple.Assets || 0
+    }
+    const damage = Math.max(sacrifices - assets, 0)
+    chara.find('a.avatar[data-id] > img').after(`<div style="text-align: center; line-height: 1em;" title="持股数 | 献祭值${damage ? ' (损耗值)' : ''}"><small>${amount} | ${sacrifices}${damage ? `(${damage})` : ''}</small></div>`)
   }
 }
