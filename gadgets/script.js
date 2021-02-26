e2717f519b2a655fec36572aebddd1c7232d5ae8
Yinr <yinr@yinr.cc>
2021-02-26 09:05:32 +0000

    Release v3.1.33

--- gadgets/script.js
+++ gadgets/script.js
@@ -5,7 +5,7 @@
 // @include     http*://bgm.tv/*
 // @include     http*://bangumi.tv/*
 // @include     http*://chii.in/*
-// @version     3.1.32
+// @version     3.1.33
 // @author      Liaune, Cedar, no1xsyzy(InQβ), Yinr
 // @homepage    https://github.com/Yinr/TinyGrail-Helper-Next
 // @license     MIT
@@ -871,10 +871,23 @@
       const charaInfo = await getData(`chara/user/${id}`);
       const amount = charaInfo.Value.Amount;
       const userId = charaInfo.Value.Id;
+      let sacrifices = 0;
+      let assets = 0;
       const templeInfo = await getData(`chara/temple/${id}`);
-      const temple = templeInfo.Value.find(i => i.UserId === userId) || {};
-      const sacrifices = temple.Sacrifices || 0;
-      chara.find('a.avatar[data-id] > img').after(`<div style="text-align: center; line-height: 1em;" title="持股数 | 献祭值"><small>${amount} | ${sacrifices}</small></div>`);
+      const temple = templeInfo.Value.find(i => i.UserId === userId);
+      if (temple === undefined) {
+        const linkInfo = await getData(`chara/links/${id}`);
+        const link = linkInfo.Value.find(i => i.UserId === userId) || {};
+        if (link !== undefined) {
+          sacrifices = link.Sacrifices || 0;
+          assets = link.Assets || 0;
+        }
+      } else {
+        sacrifices = temple.Sacrifices || 0;
+        assets = temple.Assets || 0;
+      }
+      const damage = Math.max(sacrifices - assets, 0);
+      chara.find('a.avatar[data-id] > img').after(`<div style="text-align: center; line-height: 1em;" title="持股数 | 献祭值${damage ? ' (损耗值)' : ''}"><small>${amount} | ${sacrifices}${damage ? `(${damage})` : ''}</small></div>`);
     }
   };
 
