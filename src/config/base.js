b5b7044158a0cddb7c441e0f796aa2a59253b683
Yinr <yinr@yinr.cc>
2021-03-01 21:43:43 +0800

    优化存储函数

--- src/config/base.js
+++ src/config/base.js
@@ -1,20 +1,23 @@
-const configGenerator = (name, defaultValue, config = {
-  postGet: (value) => value, // 获取设置后的过滤处理函数
-  preSet: (value) => value // 保存设置前的过滤处理函数
-}) => {
+const configGenerator = (name, defaultValue, config = {}) => {
   const storageName = `TinyGrail_${name}`
+  const { postGet, preSet, storage } = {
+    // postGet: (value) => value, // 获取设置后的过滤处理函数
+    // preSet: (value) => value, // 保存设置前的过滤处理函数
+    storage: localStorage, // 存储区域：localStorage, sessionStorage
+    ...config
+  }
   return {
     name,
     storageName,
     getRaw () {
-      return localStorage.getItem(storageName)
+      return storage.getItem(storageName)
     },
     get () {
       let value = null
       try {
         value = JSON.parse(this.getRaw())
-        if (config.postGet) {
-          value = config.postGet(value)
+        if (postGet) {
+          value = postGet(value)
         }
       } catch (err) {
         console.error(`Fail to get config of ${storageName}`, { valueString: this.getRaw(), value, err })
@@ -23,16 +26,16 @@ const configGenerator = (name, defaultValue, config = {
     },
     setRaw (valueString, raiseError = false) {
       try {
-        localStorage.setItem(storageName, valueString)
+        storage.setItem(storageName, valueString)
       } catch (err) {
         console.error(`Fail to set config of ${storageName}`, { valueString, err })
         if (raiseError) throw err
       }
     },
     set (value) {
-      if (config.preSet) {
+      if (preSet) {
         try {
-          value = config.preSet(value)
+          value = preSet(value)
         } catch (err) {
           console.warn(`Fail to preparse config of ${storageName}`, { value, err })
         }
