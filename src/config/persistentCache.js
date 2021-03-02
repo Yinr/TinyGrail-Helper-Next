e440179be12b8bf0ef4a38835e6dc0228e96ad1c
Yinr <yinr@yinr.cc>
2021-03-02 16:44:54 +0800

    增加英灵殿个人信息缓存

---
+++ src/config/persistentCache.js
@@ -0,0 +1,6 @@
+import { configGenerator } from './base'
+
+// 持久化缓存
+const PersistentCache = configGenerator('PersistentCache', {})
+
+export { PersistentCache }
--- src/ui/main/showValhallaPersonal.js
+++ src/ui/main/showValhallaPersonal.js
@@ -3,9 +3,12 @@ import { launchObserver } from '../../utils/utils'
 
 import { getSacrifices } from '../../trade/getSacrifices'
 
+import { PersistentCache } from '../../config/persistentCache'
+
 const addValhallaElement = (charaElement, amount, sacrifices, damage) => {
   const title = `持股数 | 献祭值${damage ? ' (损耗值)' : ''}，点击刷新`
   const text = `${amount} | ${sacrifices}${damage ? `(${damage})` : ''}`
+  charaElement = $(charaElement)
   const info = charaElement.find('.valhalla-sacrifices')
   if (info.length === 0) {
     charaElement.find('a.avatar[data-id] > img').after(`<div class="valhalla-sacrifices" title="${title}"><small>${text}</small></div>`)
@@ -15,7 +18,27 @@ const addValhallaElement = (charaElement, amount, sacrifices, damage) => {
 }
 
 const showValhallaItems = async (itemList) => {
-  for (let i = 0; i < itemList.length; i++) {
+  const sacrificesCache = PersistentCache.get().sacrifices || {}
+  const freshItem = []
+  const time = (new Date()).valueOf()
+  const cacheEnd = time - 24 * 60 * 60 * 1000 // 缓存时长 12 小时
+  itemList.forEach(i => {
+    const charaId = i.querySelector('a.avatar[data-id]').dataset.id
+    if (charaId in sacrificesCache) {
+      const sacrifices = sacrificesCache[charaId]
+      addValhallaElement(i, sacrifices.amount, sacrifices.sacrifices, sacrifices.damage)
+      if ((new Date(sacrifices.time)) < cacheEnd) {
+        delete sacrifices[charaId]
+        freshItem.push(i)
+      }
+    } else { freshItem.push(i) }
+  })
+  for (const [key, value] of Object.entries(sacrificesCache)) {
+    if ((new Date(value.time)) < cacheEnd) delete sacrificesCache[key]
+  }
+  PersistentCache.set({ ...PersistentCache.get(), sacrifices: sacrificesCache })
+
+  for (let i = 0; i < freshItem.length; i++) {
     const chara = $(itemList[i])
     const id = chara.find('a.avatar[data-id]').data('id')
     const charaInfo = await getData(`chara/user/${id}`)
@@ -23,7 +46,9 @@ const showValhallaItems = async (itemList) => {
 
     const { Sacrifices, Damage } = await getSacrifices(id)
     addValhallaElement(chara, amount, Sacrifices, Damage)
+    sacrificesCache[id.toString()] = { amount: amount, sacrifices: Sacrifices, damage: Damage, time: time }
   }
+  PersistentCache.set({ ...PersistentCache.get(), sacrifices: sacrificesCache })
 }
 
 export const showValhallaPersonal = () => {
