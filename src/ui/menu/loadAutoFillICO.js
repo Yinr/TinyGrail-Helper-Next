7b320cd127ea1bc3e2fb81c82cb19e66e32f762b
Yinr <yinr@yinr.cc>
2021-03-03 18:08:50 +0800

    修复自动补款列表第二页显示错误

--- src/ui/menu/loadAutoFillICO.js
+++ src/ui/menu/loadAutoFillICO.js
@@ -1,5 +1,4 @@
 import { generateCharacterList, loadCharacterList } from './loadCharacterList'
-import { loadAutoBuild } from './loadAutoBuild'
 
 import { FillICOList } from '../../config/fillICOList'
 
@@ -11,6 +10,6 @@ export const loadAutoFillICO = (page) => {
   const ids = charas.slice(start, start + 50)
   const totalPages = Math.ceil(charas.length / 50)
   generateCharacterList(ids).then(list => {
-    loadCharacterList(list, page, totalPages, loadAutoBuild, 'autofillico', false)
+    loadCharacterList(list, page, totalPages, loadAutoFillICO, 'autofillico', false)
   })
 }
